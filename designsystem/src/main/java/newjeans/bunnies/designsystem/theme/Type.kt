878b8b53294b1c0897153c55d82a1714db421edb
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-15 22:50:05 +0900

    :lipstick: :: add pretendard font

--- designsystem/src/main/java/newjeans/bunnies/designsystem/theme/Type.kt
+++ designsystem/src/main/java/newjeans/bunnies/designsystem/theme/Type.kt
@@ -1,16 +1,31 @@
 package newjeans.bunnies.designsystem.theme
 
+
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+import newjeans.bunnies.designsystem.R
+
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_bold),
+    Font(R.font.pretendard_extra_bold),
+    Font(R.font.pretendard_light),
+    Font(R.font.pretendard_medium),
+    Font(R.font.pretendard_semi_bold),
+    Font(R.font.pretendard_regular),
+    Font(R.font.pretendard_thin)
+)
+
+val authText = Typography(
+    bodyMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
