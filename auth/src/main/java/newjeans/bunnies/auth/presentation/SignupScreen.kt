ba6fefb021418796e5a6668059c484f281a13fb2
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-23 23:49:28 +0900

    :sparkles: :: add signup screen

--- auth/src/main/java/newjeans/bunnies/auth/presentation/SignupScreen.kt
+++ auth/src/main/java/newjeans/bunnies/auth/presentation/SignupScreen.kt
@@ -1,33 +1,22 @@
 package newjeans.bunnies.auth.presentation
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import newjeans.bunnies.auth.presentation.ui.image.ArrowBackImage
+
 import newjeans.bunnies.auth.viewmodel.SignupViewModel
 import newjeans.bunnies.designsystem.R
 import newjeans.bunnies.designsystem.theme.authText
-import org.w3c.dom.Text
 
 @Composable
 fun SignupScreen(
@@ -35,14 +24,13 @@ fun SignupScreen(
     onNavigateToLogin: () -> Unit
 ) {
     Row {
-        SignupAppBar()
+        SignupAppBar(onNavigateToLogin)
     }
 }
 
 @Composable
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 fun SignupAppBar(
-
+    onNavigateToLogin: () -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -50,7 +38,7 @@ fun SignupAppBar(
             .fillMaxSize(),
     ) {
         IconButton(
-            onClick = {},
+            onClick = onNavigateToLogin,
             modifier = Modifier
                 .padding(start = 30.dp)
                 .height(60.dp)
@@ -58,7 +46,9 @@ fun SignupAppBar(
             Icon(painter = painterResource(id = R.drawable.ic_arrow_back), contentDescription = "")
         }
         Text(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .height(60.dp),
             text = "회원가입",
             textAlign = TextAlign.Center,
             style = authText.headlineLarge
---
+++ auth/src/main/java/newjeans/bunnies/auth/viewmodel/SignupViewModel.kt
@@ -0,0 +1,14 @@
+package newjeans.bunnies.auth.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject
+constructor() : ViewModel() {
+
+}
