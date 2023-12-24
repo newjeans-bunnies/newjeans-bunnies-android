d5def5a69e8a67aa830ec88d6d87f7173a2c3437
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-24 23:48:58 +0900

    :lipstick: :: fix top appbar

--- auth/src/main/java/newjeans/bunnies/auth/presentation/SignupScreen.kt
+++ auth/src/main/java/newjeans/bunnies/auth/presentation/SignupScreen.kt
@@ -1,14 +1,18 @@
 package newjeans.bunnies.auth.presentation
 
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -18,6 +22,7 @@ import newjeans.bunnies.auth.viewmodel.SignupViewModel
 import newjeans.bunnies.designsystem.R
 import newjeans.bunnies.designsystem.theme.authText
 
+
 @Composable
 fun SignupScreen(
     signupViewModel: SignupViewModel,
@@ -25,6 +30,7 @@ fun SignupScreen(
 ) {
     Row {
         SignupAppBar(onNavigateToLogin)
+        Spacer(modifier = Modifier.height(50.dp))
     }
 }
 
@@ -35,7 +41,7 @@ fun SignupAppBar(
     Box(
         modifier = Modifier
             .height(60.dp)
-            .fillMaxSize(),
+            .fillMaxSize()
     ) {
         IconButton(
             onClick = onNavigateToLogin,
@@ -43,15 +49,23 @@ fun SignupAppBar(
                 .padding(start = 30.dp)
                 .height(60.dp)
         ) {
-            Icon(painter = painterResource(id = R.drawable.ic_arrow_back), contentDescription = "")
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_back),
+                contentDescription = ""
+            )
         }
-        Text(
+        Row(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .height(60.dp),
-            text = "회원가입",
-            textAlign = TextAlign.Center,
-            style = authText.headlineLarge
-        )
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "회원가입",
+                textAlign = TextAlign.Center,
+                style = authText.headlineLarge
+            )
+        }
     }
 }
