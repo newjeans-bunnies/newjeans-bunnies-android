d0a6a419a5dc33e709bdc470c99e766906324b95
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-25 23:02:15 +0900

    :zap: :: add RepositoryModule

---
+++ di/src/main/java/newjeans/bunnies/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package newjeans.bunnies.di
+
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+import newjeans.bunnies.network.auth.ApiRepositoryImpl
+import newjeans.bunnies.network.auth.AuthRepository
+
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(apiRepositoryImpl: ApiRepositoryImpl): AuthRepository =
+        apiRepositoryImpl
+
+}
