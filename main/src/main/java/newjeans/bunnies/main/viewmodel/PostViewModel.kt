0cef4affd4ef571a4506c2c38b61883a5623aa47
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-02-02 18:29:38 +0900

    :zap: :: add viewModel post api

--- main/src/main/java/newjeans/bunnies/main/viewmodel/PostViewModel.kt
+++ main/src/main/java/newjeans/bunnies/main/viewmodel/PostViewModel.kt
@@ -2,10 +2,13 @@ package newjeans.bunnies.main.viewmodel
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 
 import newjeans.bunnies.network.post.PostRepository
+import newjeans.bunnies.network.post.dto.request.MakePostRequestDto
 
 import javax.inject.Inject
 
@@ -13,6 +16,89 @@ import javax.inject.Inject
 @HiltViewModel
 class PostViewModel @Inject constructor(
     private val postRepository: PostRepository
-): ViewModel(){
+) : ViewModel() {
+    fun makePost(makePostRequestDto: MakePostRequestDto) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.makePost(makePostRequestDto)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun postGood(authorization: String, postId: String, userId: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.postGood(authorization, postId, userId)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun listPostBasicInfo(date: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.listPostBasicInfo(date)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun listPostDetail(authorization: String, date: String, userId: String){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.listPostDetail(authorization, date, userId)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun userPostBasicInfo(userId: String, date: String){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.userPostBasicInfo(userId, date)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun postDetail(authorization: String, uuid: String){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.postDetail(authorization, uuid)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+
+    fun deletePost(authorization: String, postId: String){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                postRepository.deletePost(authorization, postId)
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
 
 }
