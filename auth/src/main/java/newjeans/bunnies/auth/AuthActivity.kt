c1e1a2d608d038b07f5170654af3a98a0e5675de
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-02-24 02:16:59 +0900

    :zap: :: update firebase phoneNumberVerification

--- auth/src/main/java/newjeans/bunnies/auth/AuthActivity.kt
+++ auth/src/main/java/newjeans/bunnies/auth/AuthActivity.kt
@@ -15,6 +15,16 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -25,6 +35,7 @@ import newjeans.bunnies.auth.presentation.navigation.NavigationRoute
 import newjeans.bunnies.data.PreferenceManager
 import newjeans.bunnies.main.MainActivity
 import newjeans.bunnies.main.viewmodel.UserViewModel
+import java.util.concurrent.TimeUnit
 
 
 @AndroidEntryPoint
@@ -36,13 +47,63 @@ class AuthActivity : ComponentActivity() {
     }
 
     private val userViewModel: UserViewModel by viewModels()
+    private lateinit var auth: FirebaseAuth
+
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         prefs = PreferenceManager(this)
+        auth = Firebase.auth
 
-        FirebaseApp.initializeApp(this)
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                    // reCAPTCHA verification attempted with null Activity
+                }
+
+                // Show a message and update the UI
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken,
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+        }
+
 
         setContent {
             val navController = rememberNavController()
@@ -60,12 +121,61 @@ class AuthActivity : ComponentActivity() {
                 composable(NavigationRoute.signupRoute) {
                     SignupScreen(
                         onNavigateToLogin = { navController.navigate(NavigationRoute.loginRoute) },
-                        context = this@AuthActivity
+                        phoneNumberVerification = {
+                            startPhoneNumberVerification(changeToInternationalPhoneNumber(it))
+                        }
                     )
                 }
             }
         }
     }
+
+    private fun changeToInternationalPhoneNumber(phoneNumber: String) : String {
+        var phoneEdit = phoneNumber.substring(3)
+        phoneEdit = "+8210$phoneEdit"
+        return phoneEdit
+    }
+
+//    private fun
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        // [START start_phone_auth]
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        // [END start_phone_auth]
+    }
+
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        // [START verify_with_code]
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        // [END verify_with_code]
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    val user = task.result?.user
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+
+
 
     private fun nextActivity() {
         userViewModel.getUserDetailInformation(prefs.accessToken, prefs)
@@ -86,5 +196,14 @@ class AuthActivity : ComponentActivity() {
         applyOverrideConfiguration(override)
         super.attachBaseContext(newBase)
     }
+
+    override fun onDestroy() {
+        if (!MainActivity.prefs.autoLogin) {
+            MainActivity.prefs.deleteToken()
+            MainActivity.prefs.deleteUserData()
+        }
+        super.onDestroy()
+    }
+
 
 }
