d640dbcff766b0c8896aafe466c3fbae6f3a4c4c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-13 23:23:49 +0900

    :zap: :: add regex

--- auth/src/main/java/newjeans/bunnies/auth/Constant.kt
+++ auth/src/main/java/newjeans/bunnies/auth/Constant.kt
@@ -2,14 +2,21 @@ package newjeans.bunnies.auth
 
 object Constant {
     private val numberRegex = Regex("""^[0-9]*$""")
-    private val passwordPattern =
-        Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~․!@#\$%^&*()_\\-+=|\\\\;:‘“<>,.?/]).{10,20}\$")
+
+    private val passwordRegex =
+        Regex("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\d)(?=.*[~․!@#\$%^&*()_\\-+=|\\\\;:‘“<>,.?/]).{10,20}\$")
+
+    private val idRegex = Regex("^([a-zA-Z0-9]){1,12}\$")
 
     fun passwordPattern(input: String): Boolean {
-        return passwordPattern.matches(input)
+        return passwordRegex.matches(input)
     }
 
     fun numberPattern(input: String): Boolean {
         return numberRegex.matches(input)
+    }
+
+    fun idPattern(input: String): Boolean {
+        return idRegex.matches(input)
     }
 }
