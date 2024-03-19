789007aecb931acd4b765a32c8b5b1a2cc0fc6c6
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-19 23:23:04 +0900

    :alien: :: fix dto

--- network/src/main/java/newjeans/bunnies/network/post/dto/response/PostBasicInfoResponseDto.kt
+++ network/src/main/java/newjeans/bunnies/network/post/dto/response/PostBasicInfoResponseDto.kt
@@ -3,7 +3,9 @@ package newjeans.bunnies.network.post.dto.response
 data class PostBasicInfoResponseDto(
     val uuid: String,
     val userId: String,
+    val userImage: String,
     val body: String,
     val createDate: String,
-    val good: Long
+    val good: Long,
+    val images: List<String>
 )
--- network/src/main/java/newjeans/bunnies/network/post/dto/response/PostDetailResponseDto.kt
+++ network/src/main/java/newjeans/bunnies/network/post/dto/response/PostDetailResponseDto.kt
@@ -3,8 +3,10 @@ package newjeans.bunnies.network.post.dto.response
 data class PostDetailResponseDto(
     val uuid: String,
     val userId: String,
+    val userImage: String,
     val body: String,
     val good: Long,
     val goodStatus: Boolean,
-    val createDate: String
+    val createDate: String,
+    val images: List<String>
 )
