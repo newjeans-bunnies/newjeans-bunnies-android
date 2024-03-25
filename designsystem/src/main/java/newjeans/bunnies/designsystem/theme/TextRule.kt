8e326a4c79cec1372f9cc413468d1b90b565cacc
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-25 21:16:30 +0900

    :zap: :: add TextRule

--- designsystem/src/main/java/newjeans/bunnies/designsystem/theme/TextRule.kt
+++ designsystem/src/main/java/newjeans/bunnies/designsystem/theme/TextRule.kt
@@ -4,8 +4,9 @@ package newjeans.bunnies.designsystem.theme
 
 object TextRule {
     const val userIdMaxCharacterCount = 12
-    const val passwordMaxCharacterCount = 50
-    const val passwordMinCharacterCount = 10
+    const val userIdMinCharacterCount = 4
+    const val passwordMaxCharacterCount = 16
+    const val passwordMinCharacterCount = 8
     const val phoneNumberMaxCharacterCount = 11
     const val certificationNumberMaxCharacterCount = 6
     const val birthMaxCharacterCount = 8
